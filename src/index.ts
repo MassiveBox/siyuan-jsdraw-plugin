feb4668c87252415e17ef82765ae85ab989e256c
MassiveBox <box@massivebox.net>
2026-05-06 12:29:22 +0200

    feat: initial implementation of automatic drawing creation via shortcut

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,13 @@ import {EditorManager} from "@/editor";
 import {PluginConfig, PluginConfigViewer} from "@/config";
 import {Analytics} from "@/analytics";
 import {ErrorReporter, MustSelectError, NotAWhiteboardError} from "@/errors";
+import { sql } from './api';
 
 export default class DrawJSPlugin extends Plugin {
 
     config: PluginConfig;
     analytics: Analytics;
+    private lastActiveProtyle: Protyle | null = null;
 
     async onload() {
 
@@ -23,6 +25,10 @@ export default class DrawJSPlugin extends Plugin {
 
         await this.startConfig();
         await this.startAnalytics();
+
+        this.eventBus.on("switch-protyle", (e: any) => {
+            this.lastActiveProtyle = e.detail.protyle.getInstance();
+        });
 
         this.protyleSlash = [{
             id: "insert-whiteboard",
@@ -55,7 +61,7 @@ export default class DrawJSPlugin extends Plugin {
             langKey: "editShortcut",
             hotkey: "⌥⇧D",
             callback: async () => {
-                this.editSelectedImg().catch(e => ErrorReporter.error(e, 5000));
+                this.shortcutEditSelectedOrCreate(this.lastActiveProtyle).catch(e => ErrorReporter.error(e, 5000));
             },
         })
 
@@ -63,7 +69,8 @@ export default class DrawJSPlugin extends Plugin {
             icon: "iconDraw",
             title: this.i18n.editShortcut,
             callback: async () => {
-                await this.editSelectedImg().catch(e => ErrorReporter.error(e, 5000));
+                await this.shortcutEditSelectedOrCreate(this.lastActiveProtyle || undefined)
+                    .catch(e => ErrorReporter.error(e, 5000));
             },
             position: "left"
         })
@@ -78,11 +85,14 @@ export default class DrawJSPlugin extends Plugin {
         void this.analytics.sendEvent("uninstall");
     }
 
-    private async editSelectedImg() {
+    private async shortcutEditSelectedOrCreate(protyle?: Protyle) {
 
         let selectedImg = document.getElementsByClassName('img--select');
-        if(selectedImg.length == 0) {
-            throw new MustSelectError();
+        if(selectedImg.length == 0) { // create image
+            if (!protyle) {
+                throw new MustSelectError();
+            }
+            return this.shortcutCreate(protyle);
         }
 
         const imgSrc = findImgSrc(selectedImg[0] as HTMLElement);
@@ -96,6 +106,19 @@ export default class DrawJSPlugin extends Plugin {
         void this.analytics.sendEvent('edit');
         (await EditorManager.create(filename, this)).open(this);
 
+    }
+
+    private async shortcutCreate(protyle: Protyle) {
+        const filename = `jsdraw-${window.Lute.NewNodeID()}.svg`;
+        protyle.insert(getMarkdownBlock(filename), false, true);
+        for (let i = 0; i < 50; i++) {
+            await new Promise(r => setTimeout(r, 200));
+            const rows = await sql(`SELECT * FROM blocks WHERE content LIKE '%${filename}%'`);
+            if (rows && rows.length > 0) break;
+        }
+        void this.analytics.sendEvent('create');
+        (await EditorManager.create(filename, this)).open(this);
+        return;
     }
 
     private async startConfig() {
